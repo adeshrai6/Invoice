b97f485fd8b0510200dfd45cc202831d44b1ce4e
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-05-06 23:38:47 -0400

    Invoice

---
+++ src/main/java/com/example/Invoice.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Invoice extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Invoice.class.getResource("Invoice-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 450, 450);
+        stage.setTitle("LoginView");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,11 @@ module com.example.lab7 {
     requires javafx.fxml;
 
 
+<<<<<<< HEAD
     opens com.example.lab7 to javafx.fxml;
     exports com.example.lab7;
+=======
+    opens com.example to javafx.fxml;
+    exports com.example;
+>>>>>>> a99f39d (Invoice)
 }
